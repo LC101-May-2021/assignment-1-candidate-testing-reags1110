dfa014411459f585a3ecd50454a75ac4a762ebbb
Reags1110 <>
2021-05-24 08:17:06 +0000

    created name input, question input and checked for correct answer

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,48 +1,38 @@
 const input = require('readline-sync');
-
 // TODO 2: modify your quiz app to ask 5 questions //
-
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = 'Who was the first American woman in space?';
+let correctAnswer = 'Sally Ride';
+let candidateAnswer = '';
 let questions;
 let correctAnswers;
 let candidateAnswers;
-
-
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  candidateName = input.question('What is your name?');
+  console.log('Welcome ' + candidateName);
 }
-
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+  candidateAnswer = input.question(question);
 }
-
 function gradeQuiz(candidateAnswers) {
-
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-
   let grade;
-  
-
+  if (candidateAnswer.toLowerCase === correctAnswer.toLowerCase) {
+    console.log('Heck ya you passed!')
+  } else { console.log('Sorry please try again.')};
   return grade;
+  
 }
-
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
-
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
 module.exports = {
