9572624e9740e6c8c7824303b807307a08b1189c
Reags1110 <>
2021-06-06 06:36:34 +0000

    I made it to not only ask, but answer all of the questions and was able to respond with feedback.

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,38 +1,77 @@
 const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
+
+
 // TODO 1.1a: Define candidateName // 
-let candidateName = '';
+let candidateName = "";
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = 'Who was the first American woman in space? ';
-let correctAnswer = 'Sally Ride';
-let candidateAnswer = '';
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
+let questions = [ 'Who was the first American woman in space? ', 'True or False: 5 kilometers = 5000 meters? ', '(5 + 3)/2 * 10 = ? ', 'Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2? ', 'What is the minimum crew size for the ISS? '];
+let correctAnswers = ['Sally Ride', 'True', '40', 'Trajectory', '3'];
+let candidateAnswers = [];
+
+
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  candidateName = input.question('What is your name? ');
-  console.log('Welcome ' + candidateName);
+  name = input.question("Please enter your name: " );
 }
+
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question);
+
+let i = 0;
+ while(i < questions.length){
+  candidateAnswers.push(input.question(questions[i]));
+  console.log(`Your Answer: ${candidateAnswers[i]}\nCorrectAnswer: ${correctAnswers[i]}\n`);
+  i++;
 }
+}
+
 function gradeQuiz(candidateAnswers) {
+ 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  let grade;
-  if (candidateAnswer.toUpperCase() === correctAnswer.toUpperCase()) {
-    console.log('Heck ya you passed!')
-  } else { console.log('Sorry please try again.')};
-  return grade;
   
-}
+  candidateScore = 0;
+  maxScore = 5;
+
+  if (candidateAnswers[0].toLowerCase() === correctAnswers[0].toLowerCase()){
+    candidateScore += 1;
+  }
+  if (candidateAnswers[1].toLowerCase() === correctAnswers[1].toLowerCase()){
+    candidateScore += 1;
+  }
+  if (candidateAnswers[2].toLowerCase() === correctAnswers[2].toLowerCase()){
+    candidateScore += 1;
+  }
+  if (candidateAnswers[3].toLowerCase() === correctAnswers[3].toLowerCase()){
+    candidateScore += 1;
+  }
+  if (candidateAnswers[4].toLowerCase() === correctAnswers[4].toLowerCase()){
+    candidateScore += 1;
+  }
+
+  let grade = candidateScore / maxScore * 100;
+
+console.log(`>>> Final Score: ${grade} % (${candidateScore} / ${maxScore} were Correct)<<< `);
+  if (grade >= 80) {
+    console.log(`>>> Congratulations! You PASSED!!! <<<`);
+  } else {
+    console.log(">>> Sorry, you have FAILED! <<<")
+  }
+  return grade;
+  }
+
 function runProgram() {
-  // TODO 1.1c: Ask for candidate's name //
-  askForName(candidateName);
-  askQuestion(question);
+  askForName();
+  console.log(`Hello ${name}!` );
+  askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
+
+
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
 module.exports = {
